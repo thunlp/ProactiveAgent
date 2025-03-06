e9d8f478650b9aef06ef13454c9d234a5f2d616b
SummerFall1819 <summerfall1819@gmail.com>
2025-03-06 10:25:12 +0800

    Small fix for #29

--- gym/main.py
+++ gym/main.py
@@ -12,15 +12,15 @@ from .channel import sinkChannels
 async def data_loop(cfg_file: str,out_file: Optional[str] = None):
     with open(cfg_file, 'r') as f:
         cfg = yaml.safe_load(f)
-    
+
     if out_file is None:
         out_file = cfg['eventSink'].get("out_file", uuid.uuid4().hex + ".jsonl")
-    
+
     if os.path.exists(out_file):
         os.remove(out_file)
-    
+
     out = open(out_file,"x")
-    
+
     def decorator(func):
         def wrapped_add(*args,**kwargs):
             ret = func(*args,**kwargs)
@@ -36,21 +36,21 @@ async def data_loop(cfg_file: str,out_file: Optional[str] = None):
 
     env = EnvironmentStateManager(**cfg["environment"])
     user = UserAgent(**cfg["user"])
-    if os.environ.get("SETUP_PROACTIVE_AGENT","True") == "True":
+    if os.environ.get("SETUP_PROACTIVE_AGENT","False") == "True":
         agent = ProactiveAgent(**cfg["agent"])
 
     # wait Setup
     eventSink.add(tags=sinkChannels.setup, content="Setup Components...",)
     await eventSink.wait(sinkChannels.setup)
     eventSink.add(tags=sinkChannels.setup,
-                  content="Setup Completed!", silent=True)
+                    content="Setup Completed!", silent=True)
 
     logger.info("*** Components setup completed. ***")
     # setup environment adapation
     await env.intro()
     await eventSink.wait(sinkChannels.env.all)
     logger.info("*** Environment Adaptation Completed. ***")
-    
+
     # start activity and events generation
     await user.step()
 
